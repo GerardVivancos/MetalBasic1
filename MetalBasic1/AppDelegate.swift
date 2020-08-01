1b1b58d79a4ad90d24c0c7895363b3e95ff6eccc
Gerard Vivancos <gerard.vivancos@gmail.com>
2020-08-01 20:06:06 +0200

    Initial Commit

---
+++ MetalBasic1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MetalBasic1
+//
+//  Created by garbage on 01/08/2020.
+//  Copyright © 2020 tmcg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MetalBasic1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MetalBasic1
+//
+//  Created by garbage on 01/08/2020.
+//  Copyright © 2020 tmcg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
